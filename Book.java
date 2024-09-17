04f8093bd725eb442da3e0d5361ae88aa623a198
RC-COOLA <a2jministries@gmail.com>
2024-09-17 22:50:56 +0000

    Charles

---
+++ Book.java
@@ -0,0 +1,39 @@
+public class Book 
+
+{
+    String title; 
+    String price; 
+    Object Author;
+
+    public char Book (String title, String price, String Author)
+    {
+
+    this.title= "Tuck_Everlasting";
+    this.price= "19.99";
+    this.Author= "Natalie_Babbitt";
+
+    public static void main(String[] args) {
+
+        system.out.println ("title","price", "author:"\n);
+}
+
+public class author 
+
+ {
+    
+    String Name;
+    String Email;
+    String Nationality;
+
+    public author (String Name, String Email, String Nationality)
+{
+    this.Name = "Natalie_Babbitt";
+    this.Email = "BN@gamil.com";
+    this.Nationality = "American";
+    }
+    public static void main(String[] args) {
+
+        system.out.println ("Name:", "Email", "Nationality"\n);
+    }
+return ();
+}
